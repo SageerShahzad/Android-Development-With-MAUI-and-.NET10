1d34379bc108096aa74fdbe49623e8cb7d44b3e6
SageerS <SageerS@KaliLinuxCode.com>
2025-12-29 06:29:15 +0000

    Update chat message text input

--- ClassifiedAds.Mobile/Platforms/Android/MainActivity.cs
+++ ClassifiedAds.Mobile/Platforms/Android/MainActivity.cs
@@ -1,12 +1,15 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;  // Required for SoftInput
 
 namespace ClassifiedAds.Mobile
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
-        WindowSoftInputMode = Android.Views.SoftInput.AdjustResize,
-          ConfigurationChanges = ConfigChanges.ScreenSize)]
+    [Activity(Theme = "@style/Maui.SplashTheme",
+              MainLauncher = true,
+              LaunchMode = LaunchMode.SingleTop,
+              WindowSoftInputMode = SoftInput.AdjustResize | SoftInput.StateHidden,  // StateHidden prevents auto-popup on load
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
